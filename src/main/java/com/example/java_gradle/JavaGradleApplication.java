9dcc758128b0262a77d0b13c3f249c29a7b8d3b3
Chamika Madhushan <chamikamadhushan35@gmail.com>
2024-12-15 08:07:18 +0530

    init

---
+++ src/main/java/com/example/java_gradle/JavaGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGradleApplication.class, args);
+	}
+
+}
